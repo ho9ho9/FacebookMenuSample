9884b45e54bc8595e67fb04d18d92b3e257ec945
ho9ho9 <ho9ho9.developer@gmail.com>
2012-10-17 00:50:49 +0900

    Initial Commit

---
+++ FacebookMenuSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FacebookMenuSample
+//
+//  Created by ho9ho9 on 2012/10/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FacebookMenuSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FacebookMenuSample
+//
+//  Created by ho9ho9 on 2012/10/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
