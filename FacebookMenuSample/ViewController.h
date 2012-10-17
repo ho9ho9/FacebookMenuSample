24f91929b8a5e5c461a2d7e75e7004cad204fe3c
ho9ho9 <ho9ho9.developer@gmail.com>
2012-10-18 01:39:59 +0900

    commit

--- FacebookMenuSample/ViewController.h
+++ FacebookMenuSample/ViewController.h
@@ -8,10 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController {
+@interface ViewController : UIViewController<UITableViewDataSource, UITableViewDelegate> {
     @private
     __weak IBOutlet UIScrollView *_scrollView;
     __weak IBOutlet UIView *_mainView;
+    __weak IBOutlet UITableView *_menuTable;
+    __weak IBOutlet UINavigationBar *_navigationBar;
 }
 
 - (IBAction)tapMenu:(id)sender;
--- FacebookMenuSample/ViewController.m
+++ FacebookMenuSample/ViewController.m
@@ -22,15 +22,19 @@
     float defWidth = _scrollView.frame.size.width;
     
     _scrollView.pagingEnabled = YES;
-    _scrollView.contentSize = CGSizeMake(defWidth + (defWidth * 0.8), _scrollView.frame.size.height);
+    _scrollView.contentSize = CGSizeMake(defWidth + 260, _scrollView.frame.size.height);
     _scrollView.showsHorizontalScrollIndicator = NO;
     _scrollView.showsVerticalScrollIndicator = NO;
     _scrollView.scrollsToTop = NO;
     _scrollView.bounces = NO;
 //    _scrollView.delegate = self;
-    _scrollView.contentOffset = CGPointMake((defWidth * 0.8), _scrollView.contentOffset.y);
+    _scrollView.contentOffset = CGPointMake(260, _scrollView.contentOffset.y);
     
-    _mainView.frame = CGRectMake((defWidth * 0.8), _scrollView.contentOffset.y, _mainView.frame.size.width, _mainView.frame.size.height);
+    _mainView.frame = CGRectMake(260, _scrollView.contentOffset.y, _mainView.frame.size.width, _mainView.frame.size.height);
+    
+    _menuTable.frame = CGRectMake(0, _scrollView.contentOffset.y, 260, _mainView.frame.size.height);
+    _menuTable.delegate = self;
+    _menuTable.dataSource = self;
 }
 
 - (void)didReceiveMemoryWarning
@@ -39,16 +43,65 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)tapMenu:(id)sender {
+-(void) swipeMenu {
     [UIView beginAnimations:nil context:nil];
-    [UIView setAnimationDuration:0.2];
+    [UIView setAnimationDuration:0.15];
     [UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
     if (_scrollView.contentOffset.x == 0) {
-        _scrollView.contentOffset = CGPointMake((_scrollView.frame.size.width * 0.8), _scrollView.contentOffset.y);
+        _scrollView.contentOffset = CGPointMake(260, _scrollView.contentOffset.y);
     } else {
         _scrollView.contentOffset = CGPointMake(0, _scrollView.contentOffset.y);
     }
     [UIView commitAnimations];
+}
+
+- (IBAction)tapMenu:(id)sender {
+    [self swipeMenu];
+}
+
+#pragma mark - UITableViewDelegate
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    [_menuTable deselectRowAtIndexPath:indexPath animated:YES];
+    _navigationBar.topItem.title = [NSString stringWithFormat:@"タイトル[%d-%d]", indexPath.section, indexPath.row];
+    [self swipeMenu];
+}
+#pragma mark - UITableViewDataSource
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    NSInteger count = 0;
+    if (section == 0) {
+        count = 3;
+    } else {
+        count = 5;
+    }
+    return count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [_menuTable dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    NSString *cellValue = [NSString stringWithFormat:@"メニュー [%d-%d]", indexPath.section, indexPath.row];
+    cell.textLabel.text = cellValue;
+    
+    return cell;
+}
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
+}
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    NSString *title = nil;
+    if (section == 0) {
+        title = @"MenuCategory α";
+    } else {
+        title = @"MenuCategory β";
+    }
+    return title;
 }
 
 @end
